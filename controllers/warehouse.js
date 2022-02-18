52d77080fb0853b22953506b1ebbcc80248a3aa7
Imad Arrada <imad-seddik.arrada@capgemini.com>
2022-02-18 15:13:57 +0100

    Routes of listing warehouses + creation of warehouse

--- controllers/warehouse.js
+++ controllers/warehouse.js
@@ -1,13 +1,47 @@
 const utils = require('../utils/utils');
-// const fs = require('fs');
-//const ContactSchema = require('../model/model');
+const WarehouseSchema = require('../models/warehouse');
 
 const warehouseController = {
-    listWarehouses: (req, res) => {
-        res.set({"Access-Control-Allow-Origin" : "*", 
-        "Access-Control-Allow-Credentials" : true });
-        res.status(200).json(utils.warehouses);
+    /**
+     * Methode: POST
+     * @param {*} req 
+     * @param {*} res 
+     */
+    createWarehouse: (req, res) => {    
+        try {
+            const warehouse = new WarehouseSchema({...req.body, creatorId: req.userId, appointments: []});
+            warehouse.save()
+                .then(() => res.status(201).json({ ok: true, message: "Warehouse successfully created" }))
+                .catch((error) => res.status(400).json({ ok: false, message: "Error while saving data", error: error }));
+        } catch (error){
+            res.status(500).json({ ok: false, message: error });
+        }
     },
+
+    /**
+     * Methode: GET
+     * @param {*} req 
+     * @param {*} res 
+     */
+    listMyWarehouses: async (req, res) => {
+        const warehouses = await WarehouseSchema.find({creatorId: String(req.userId)});
+        if (warehouses) {
+            res.status(200).json({ok: true, warehouses: warehouses});
+        } else if (warehouses.length === 0) {
+            res.status(204).json({ok: true, warehouses: []})
+        } else {
+            res.status(500).json({ok: false, message: "Error while fetching warehouse data"})
+        }
+    },
+
+    /**
+     * Methode: POST
+     * @param {*} req 
+     * @param {*} res 
+     */
+    listAllWarehouses: async (req, res) => {
+        
+    }
 }
 
 module.exports = warehouseController;
--- routes/warehouse.js
+++ routes/warehouse.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const warehouseController = require('../controllers/warehouse');
+const auth = require('../middlewares/auth');
 
-/* GET users listing. */
-router.get('/', warehouseController.listWarehouses);
+/**
+ * Methode: GET
+ * Role: Gestionnaire
+ * Lists the warehouses of a Gestionnaire
+ * 
+ *  */ 
+router.get('/', auth, warehouseController.listMyWarehouses);
+
+/**
+ * Methode: POST
+ * Role: Gestionnaire
+ * Creates a new warehouse
+ * 
+ */
+
+router.post('/add', auth, warehouseController.createWarehouse);
+
+/**
+ * Methode: POST
+ * Role: Livreur
+ * Search page of all warehouses of all gestionnaires
+ */
+router.post('/search', auth, warehouseController.listAllWarehouses)
+
 
 module.exports = router;
